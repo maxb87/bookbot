2ef0a747fbf2283671df91df8bf5d689a60e620b
maxb87 <bretschneider.m@gmail.com>
2025-01-15 15:03:54 -0300

    Report now prints the ordered alpha character count

--- main.py
+++ main.py
@@ -26,9 +26,10 @@ def generate_report(book_path):
   text = get_book_text(path)
   word_count = get_num_words(text)
   char_count = get_character_count(text)
+  ordered_char_count = {k: char_count[k] for k in sorted(char_count.keys())}
   print(f"{word_count} words found in the document.")
   print("# List of characters and ocurrances in the document.")
-  for key,value in char_count.items():
+  for key,value in ordered_char_count.items():
     print(f"- The character \"{key}\" has {value} ocurrances.")
-
+  
 main()
