4f3006b3726ce1e8ef813451f377f8906128d13a
maxb87 <bretschneider.m@gmail.com>
2025-01-15 14:02:30 -0300

    Working word count and character count

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-def read_book():
-  with open("./books/frankenstein.txt") as f:
+def main():
+  book_path = "books/frankenstein.txt"
+  text = get_book_text(book_path)
+  num_words = get_num_words(text)
+  char_count =  get_character_count(text)
+  print(f"{num_words} words found in the document")
+  print(f"{char_count}")
+
+def get_num_words(text):
+  words = text.split()
+  return len(words)
+
+def get_character_count(text):
+  char_count = {}
+  lowercase_string = text.lower()
+  
+  for char in lowercase_string:
+    if char.isalpha():
+      char_count[char] = char_count.get(char, 0) + 1
+  
+  return char_count
+    
+
+def get_book_text(path):
+  with open(path) as f:
     return f.read()
 
-def word_count(text):
-  text = read_book()
-  words = text.split()
-  return words.count
-
-def main():
-  print(word_count(read_book()))
-
-if __name__ == "__main__":
-  main()
+main()
