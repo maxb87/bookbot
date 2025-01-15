7079a53c780caf0f7ade2f03c840e53558289801
maxb87 <bretschneider.m@gmail.com>
2025-01-15 14:47:38 -0300

    Word and character count reports

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def main():
   book_path = "books/frankenstein.txt"
-  text = get_book_text(book_path)
-  num_words = get_num_words(text)
-  char_count =  get_character_count(text)
-  print(f"{num_words} words found in the document")
-  print(f"{char_count}")
+  generate_report(book_path)
 
 def get_num_words(text):
   words = text.split()
@@ -19,10 +15,20 @@ def get_character_count(text):
       char_count[char] = char_count.get(char, 0) + 1
   
   return char_count
-    
 
 def get_book_text(path):
   with open(path) as f:
     return f.read()
+  
+def generate_report(book_path):
+  path = book_path
+  print(f"### Word and Character count report on file {path} ###")
+  text = get_book_text(path)
+  word_count = get_num_words(text)
+  char_count = get_character_count(text)
+  print(f"{word_count} words found in the document.")
+  print("# List of characters and ocurrances in the document.")
+  for key,value in char_count.items():
+    print(f"- The character \"{key}\" has {value} ocurrances.")
 
 main()
